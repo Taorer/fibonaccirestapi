c4b518b01bcb0c05ce55e335ccd7bc58a06d3629
Tibi <miniszoko2@gmail.com>
2025-09-08 12:17:18 +0200

    DevOps Gyak

--- src/main/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciService.kt
+++ src/main/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciService.kt
@@ -5,7 +5,22 @@ import org.springframework.stereotype.Service
 @Service
 class FibonacciService {
     fun fibonacci(n: Int): Int {
-        return if (n == 1) 0
-        else 0 // TODO instead of this logic implement fibonacci
+        if ( n <= 0) return -1
+        if ( n > 46) return -1
+
+        if (n == 1) return 0
+        if (n == 2) return 1
+
+        var a = 0
+        var b = 1
+        var result = 0
+
+        for (i in 3..n) {
+            result = a + b
+            a = b
+            b = result
+        }
+
+        return result
     }
 }
